0f3e72efda0073562b8a1ca3452070482c582865
Evanilson freitas <evanilson.2631@hotmail.com>
2022-08-21 23:51:23 -0300

    Push
    Push 1.0

---
+++ Relógio/js/app.js
@@ -0,0 +1,18 @@
+const horas = document.getElementById('horas')
+const minuto = document.getElementById('min')
+const segundos = document.getElementById('seg')
+
+const time = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if(hr < 10) hr = '0' + hr
+    if(min < 10) min = '0' + min
+    if(seg < 10) seg = '0' + seg
+
+    horas.textContent = hr;
+    minuto.textContent = min;
+    segundos.textContent = seg;
+})
